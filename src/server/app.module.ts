fc4335040ec35a8c9110ffdcd957307e04b47209
Brian Kopp <briankopp.usa@gmail.com>
2022-06-26 16:42:12 -0600

    Dynamically import uinext module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UiNextModule } from './ui-next/ui-next.module';
 
 @Module({
-  imports: [UiNextModule],
+  imports: [UiNextModule.initialize()],
   controllers: [],
   providers: [],
 })
