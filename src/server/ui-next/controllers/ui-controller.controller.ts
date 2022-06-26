6477a4fa9ed4a4c2593d309d2c5691ab7020916b
Brian Kopp <briankopp.usa@gmail.com>
2022-06-26 15:57:16 -0600

    use type declaration

--- src/server/ui-next/controllers/ui-controller.controller.ts
+++ src/server/ui-next/controllers/ui-controller.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import { UiRenderService } from '../services/ui-render.service';
 
 @Controller('/')
