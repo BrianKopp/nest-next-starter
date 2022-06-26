dda603f60be41b70a5052dc4e34a405ba92fbbf1
Brian Kopp <briankopp.usa@gmail.com>
2022-06-26 15:50:05 -0600

    Remove boilerplate controller and service

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UiNextModule } from './ui-next/ui-next.module';
 
 @Module({
   imports: [UiNextModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
