7510dcc929bf0626f217cc54c36c4e41fd66a54a
Brian Kopp <briankopp.usa@gmail.com>
2022-06-26 16:42:03 -0600

    Add hot module rendering to nest

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SOMETHING } from '../shared/something';
 
+// eslint-disable-next-line @next/next/no-assign-module-variable
+declare const module: any;
+
 console.log(SOMETHING);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
