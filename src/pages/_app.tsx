589d0b310e9b40d338fe63b0d21940895254874e
Brian Kopp <briankopp.usa@gmail.com>
2022-06-26 16:25:11 -0600

    Set up basic tailwind usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
+import '../client/styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
